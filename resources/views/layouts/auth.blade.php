f8e69f6bab82beb225989a40f87b203fe28caa8e
CharlySoftCO <cramirezruiz30@gmail.com>
2025-01-05 19:36:42 -0500

    Initial commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title') - {{ config('app.name') }}</title>
+
+    <!-- Scripts -->
+    @viteReactRefresh
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body class="hold-transition login-page">
+    @yield('content')
+</body>
+
+</html>
